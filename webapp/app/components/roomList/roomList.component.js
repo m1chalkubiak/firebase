b593b3949b37db8d21643f2408be6cff20d69f70
Michał Kubiak <mkubiak@apptension.com>
2018-03-01 21:06:32 +0100

    Refactor renderList function to use curring

--- webapp/app/components/roomList/roomList.component.js
+++ webapp/app/components/roomList/roomList.component.js
@@ -40,11 +40,11 @@ export class RoomListComponent extends PureComponent {
   );
 
   renderList = () => {
-    const isActive = (id) => ifElse(
+    const isActive = ifElse(
       equals(this.props.activeRoom.get('id')),
       always({ active: 'active' }),
       always(null)
-    )(id);
+    );
 
     return this.props.rooms.sort().map((room) =>
       <RoomLink to={room.get('_id')} key={room.get('_id')} {...isActive(room.get('_id'))}>
