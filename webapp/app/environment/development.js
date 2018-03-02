bf43d5f6781f27cf9ff0c5ff35b1ae26966af8e0
Michał Kubiak <mkubiak@apptension.com>
2018-03-02 11:53:46 +0100

    Remove API data from repo

--- webapp/app/environment/development.js
+++ webapp/app/environment/development.js
@@ -3,11 +3,5 @@ import buildConfig from '../utils/buildConfig';
 export default buildConfig({
   name: 'development',
   firebaseConfig: {
-    apiKey: 'AIzaSyC02DN5a3ayCm3a8bsu3hiXomto-I-CNtA',
-    authDomain: 'fir-chat-9dd5e.firebaseapp.com',
-    databaseURL: 'https://fir-chat-9dd5e.firebaseio.com',
-    projectId: 'fir-chat-9dd5e',
-    storageBucket: 'fir-chat-9dd5e.appspot.com',
-    messagingSenderId: '700350008629',
   },
 });
