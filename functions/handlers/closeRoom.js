79d15ba3e0acd2fd416139452f94ad2629456e70
Wojciech Panek <wpanek@apptension.com>
2018-03-09 13:26:49 +0100

    Replace .set(null) with .remove()

--- functions/handlers/closeRoom.js
+++ functions/handlers/closeRoom.js
@@ -3,5 +3,5 @@ const admin = require('firebase-admin');
 
 exports.deleteHandler = (event) => {
   const { roomId } = event.params;
-  return admin.database().ref('/messages').child(roomId).set(null);
+  return admin.database().ref('/messages').child(roomId).remove();
 };
