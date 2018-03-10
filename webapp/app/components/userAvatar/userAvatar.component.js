77f66eede3be6be7aca5b806c0217ba7bb1f45a0
Michał Kubiak <mkubiak@apptension.com>
2018-03-10 21:24:38 +0100

    Add users list to menu drawer

--- webapp/app/components/userAvatar/userAvatar.component.js
+++ webapp/app/components/userAvatar/userAvatar.component.js
@@ -1,20 +1,42 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Avatar from 'material-ui/Avatar';
 import { Map } from 'immutable';
+import { always, ifElse, identity, equals, complement } from 'ramda';
 
+import { Wrapper } from './userAvatar.styles';
 
 export class UserAvatar extends PureComponent {
   static propTypes = {
     user: PropTypes.instanceOf(Map),
+    small: PropTypes.bool,
+    status: PropTypes.string,
   };
 
   static defaultProps = {
     user: Map(),
+    small: false,
   };
 
+  get isSmall() {
+    return ifElse(
+      identity,
+      always({ small: true }),
+      always(null),
+    )(this.props.small);
+  }
+
+  get isOffline() {
+    return ifElse(
+      complement(equals('online')),
+      always({ offline: true }),
+      always(null),
+    )(this.props.status);
+  }
+
   render = () => (
-    <Avatar
+    <Wrapper
+      {...this.isOffline}
+      {...this.isSmall}
       alt={this.props.user.get('displayName', '')}
       src={this.props.user.get('profilePhoto', '')}
     />
---
+++ webapp/app/components/usersList/usersList.component.js
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Map } from 'immutable';
+import { injectIntl } from 'react-intl';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import PeopleIcon from 'material-ui-icons/People';
+
+import { UserAvatar } from '../userAvatar/userAvatar.component';
+import messages from './usersList.messages';
+import { UserItem  } from './usersList.styles';
+
+
+export class UsersListComponent extends PureComponent {
+  static propTypes = {
+    intl: PropTypes.object.isRequired,
+    users: PropTypes.instanceOf(Map),
+  };
+
+  static defaultProps = {
+    users: Map(),
+  };
+
+  renderRoomLabel = () => (
+    <ListItem>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary={this.props.intl.formatMessage(messages.listLabel)} />
+    </ListItem>
+  );
+
+  renderList = () => this.props.users.sort().map((user) =>
+    <UserItem key={user.get('_id')} >
+      <UserAvatar user={user.get('value')} small />
+      <ListItemText primary={user.getIn(['value', 'displayName'])} />
+    </UserItem>
+  ).toArray();
+
+  render = () => (
+    <List component="nav">
+      {this.renderRoomLabel()}
+      {this.renderList()}
+    </List>
+  );
+}
+
+export const UsersList = injectIntl(UsersListComponent);
