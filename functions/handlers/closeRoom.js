71c70f0f216fb4f8fb2b6a1e6aa2fb39768c4e81
Wojciech Panek <wpanek@apptension.com>
2018-03-10 11:37:47 +0100

    Reformat deleteHandler

--- functions/handlers/closeRoom.js
+++ functions/handlers/closeRoom.js
@@ -1,7 +1,4 @@
 const admin = require('firebase-admin');
 
 
-exports.deleteHandler = (event) => {
-  const { roomId } = event.params;
-  return admin.database().ref('/messages').child(roomId).remove();
-};
+exports.deleteHandler = ({ params: { roomId } }) => admin.database().ref('/messages').child(roomId).remove();
